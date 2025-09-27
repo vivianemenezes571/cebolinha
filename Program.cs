86ebc6b50a229f1533005faa63d27ec605820e36
vivianemenezes571 <vivianemenezes5711@gmail.com>
2025-09-26 21:29:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string fraseDigitada, fraseComDislalia;
+
+Console.Write("Digite uma flase: ");
+fraseDigitada = Console.ReadLine()!;
+
+fraseComDislalia = fraseDigitada
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.WriteLine(fraseComDislalia);
