e985f1ad8567f249fc83922d5c245d319e295684
vivianemenezes571 <vivianemenezes5711@gmail.com>
2025-09-26 21:49:21 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿string fraseDigitada, fraseComDislalia;
 
-Console.Write("Digite uma flase: ");
+Console.Write("Digite uma frase: ");
 fraseDigitada = Console.ReadLine()!;
 
 fraseComDislalia = fraseDigitada
     .Replace("r", "l")
     .Replace("R", "L");
+
 
 Console.WriteLine(fraseComDislalia);
